d7296cd6b4fcf5a9432d1df4c3a92305ded08ef3
MD. SHARIF UDDIN <101212925+sharif4u@users.noreply.github.com>
2022-05-27 15:18:25 +0600

    SorthingAssendingAnDesending.cpp

---
+++ SorthingAssendingAnDesending.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j, k, a[5]={4,5,1,10,7}, n=5, temp;
+        for(i = 0; i < n; i++){
+            j = i;
+            for(k = i+1; k < n; k++){
+                if(a[k] < a[j]){
+                    temp = a[j];
+                    a[j] = a[k];
+                    a[k] = temp;
+                }
+            }
+        }
+        cout << "After asending sorting is: ";
+        for(i = 0; i < n; i++){
+            cout << a[i] << " ";
+        }
+
+        for(i = 0; i < n; i++){
+            j = i;
+            for(k = i+1; k < n; k++){
+                if(a[k] > a[j]){
+                    temp = a[j];
+                    a[j] = a[k];
+                    a[k] = temp;
+                }
+            }
+        }
+        cout << "\nAfter desending sorting is: ";
+        for(i = 0; i < n; i++){
+            cout << a[i] << " ";
+        }
+
+
+    return 0;
+}
+
